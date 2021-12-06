6d07cb532eddbdfdbaef565c20b019760433d99c
Isaac <isaac.k.rossatto@gmail.com>
2021-12-06 16:26:39 -0300

    msg erro

--- src/app/views/cadastro-jogos/cadastro-jogos.component.ts
+++ src/app/views/cadastro-jogos/cadastro-jogos.component.ts
@@ -5,11 +5,55 @@ import { FormBuilder, FormGroup, Validators } from "@angular/forms";
   templateUrl: './cadastro-jogos.component.html',
   styleUrls: ['./cadastro-jogos.component.css']
 })
-export class CadastroJogosComponent implements OnInit {
+export class CadastroJogosComponent {
 
-  constructor() { }
 
-  ngOnInit(): void {
+  exibir(){
+    let id: string = (<HTMLInputElement>document.getElementById("inputId")).value;
+
+    if(id == ""){
+      alert("preencha o campo ID")
+    }else{
+      alert("você clicou em EXIBIR")
+    }
+  };
+  cadastrar(){
+    let id: string = (<HTMLInputElement>document.getElementById("inputId")).value;
+    let nome: string = (<HTMLInputElement>document.getElementById("inputNome")).value;
+    let tipo: string = (<HTMLInputElement>document.getElementById("inputTipo")).value;
+    let empresa: string = (<HTMLInputElement>document.getElementById("inputEmpresa")).value;
+
+    if(id ==""){
+      alert("Preencha todos os CAMPOS");
+    }else if (nome ==""){
+      alert("Preencha todos os CAMPOS");
+    }else if (tipo ==""){
+      alert("Preencha todos os CAMPOS");
+    }else if (empresa ==""){
+      alert("Preencha todos os CAMPOS");
+    }else{
+      alert("você clicou em CADASTRAR")
+    }
+  };
+  atualizar(){
+    let nome: string = (<HTMLInputElement>document.getElementById("inputNome")).value;
+
+    if (nome ==""){
+      alert("preencha o Nome");
+    }else{
+      alert("você clicou em ATUALIZAR")
+    }
+  };
+  deletar(){
+    let id: string = (<HTMLInputElement>document.getElementById("inputId")).value;
+    
+    
+  
+  if (id ==""){
+    alert("preencha pelomenos o ID do jogo");
+  }else{
+    alert("você clicou em DELETAR")
   }
+};
 
 }
